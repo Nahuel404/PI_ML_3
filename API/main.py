bbeed0146c4de9739a142afc8822ba048ad45def
Nahuel404 <nahuelmarincivil@gmail.com>
2023-07-11 14:35:29 -0300

    0.2.0

--- API/main.py
+++ API/main.py
@@ -111,6 +111,6 @@ def recomendacion(pelicula):
 
     return respuesta
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
